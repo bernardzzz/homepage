dcd31d09bb2f9a1186337253bf3bf6b58c303361
Bernard Wong <bernard@regrow.ag>
2026-07-18 23:26:41 +1000

    Migrate hosting from AWS to Cloudflare Pages
    - Removed reCAPTCHA backend and S3 upload script
    - Simplified index.html to use Cloudflare email obfuscation
    - Added GitHub Actions workflow to deploy to Cloudflare Pages

--- upload-to-s3.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bucket_name="bernard-homepage-website"
-
-aws s3 cp --recursive . s3://${bucket_name}/ --exclude "*.sh" --exclude "*.js" --exclude "*.md" --exclude ".git/*" --exclude ".idea/*"
